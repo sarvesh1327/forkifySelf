d8a73cda948071eef297eb84ced6dc898e4e7b55
Sarvesh Agarwal <32209706+sarvesh1327@users.noreply.github.com>
2021-10-08 14:36:53 +0530

    Delete src/js directory

--- src/js/config.js
+++
@@ -1,4 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE=10;
-export const KEY = '08758246-5e09-48bc-af02-7e344133ff2e';
